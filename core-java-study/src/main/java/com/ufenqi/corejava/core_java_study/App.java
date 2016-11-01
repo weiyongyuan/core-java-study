c945ef21059efa7d203f487ae137b023b224995e
weiyongyuan <weiyongyuan@aliyun.com>
2016-11-01 23:51:08 +0800

    diyici

---
+++ core-java-study/src/main/java/com/ufenqi/corejava/core_java_study/App.java
@@ -0,0 +1,13 @@
+package com.ufenqi.corejava.core_java_study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
